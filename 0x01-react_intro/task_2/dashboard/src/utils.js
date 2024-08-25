789d586ef65f1eaa97a3319b3e260f4bfc0e42f8
Dahesey <dahesey@gmail.com>
2024-08-25 15:41:50 +0000

    Added Notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,3 +1,7 @@
 export const getFullyear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+
+export const getLatestNotification = () => {
+    return "<strong>Urgent requirement</strong> - complete by EOD";
+};
